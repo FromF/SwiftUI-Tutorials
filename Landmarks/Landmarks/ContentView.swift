fe5e473ee37288bf0fa39c5b40ad8c1547cfe7ff
Haruhito Fuji <haruhito.fuji@gmail.com>
2019-06-12 19:17:35 +0900

    プロジェクトデータ作成

---
+++ Landmarks/Landmarks/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by 藤治仁 on 2019/06/12.
+//  Copyright © 2019 FromF.github.com. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
